187eff2da58be2a04411bc0cdd373eeb6f190c28
saidmusabk <78953454+saidmusabk@users.noreply.github.com>
2022-06-24 23:05:43 +0300

    Add files via upload

---
+++ javaCampOdevler/yayin2Odev1/src/CampManager.java
@@ -0,0 +1,8 @@
+public class CampManager {
+    public void joinCamp(Camp camp){
+        System.out.println("Kaydınız tamamlandı.");
+    }
+    public void quitCamp(Camp camp) {
+        System.out.println(camp.name + "adlı kamptan kaydınız silinmiştir.");
+    }
+}
